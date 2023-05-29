24645f3a3464920234b22044aab84afc4d1d4260
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-05-29 11:50:17 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -28,7 +28,7 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001596651023"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001802315857"))
 
 # A name for your Music bot.
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","Camellia")
